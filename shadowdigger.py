99cafd20b02ff56d4dbaaff402bde9588a8e1cb1
Sethro <93166687+S3THR0@users.noreply.github.com>
2023-07-07 17:36:39 -0400

    Update shadowdigger.py
    New features added

--- shadowdigger.py
+++ shadowdigger.py
@@ -29,13 +29,25 @@ def create_table(conn):
         cursor.execute('''CREATE TABLE IF NOT EXISTS entries (
                           id INTEGER PRIMARY KEY,
                           full_name TEXT NOT NULL,
-                          main_usernames TEXT,
+                          date_of_birth TEXT,
+                          phone_number TEXT,
+                          email TEXT,
                           home_address TEXT,
                           city TEXT,
                           zip_postal TEXT,
                           state_province TEXT,
                           country TEXT,
-                          data TEXT
+                          facebook TEXT,
+                          twitter TEXT,
+                          instagram TEXT,
+                          linkedin TEXT,
+                          work_job_name TEXT,
+                          work_address TEXT,
+                          work_position TEXT,
+                          other_work_info TEXT,
+                          main_usernames TEXT,
+                          data TEXT,
+                          image BLOB
                           );''')
         print('Table created successfully')
     except Error as e:
@@ -43,8 +55,10 @@ def create_table(conn):
 
 def insert_entry(conn, entry):
     try:
-        sql = ''' INSERT INTO entries(full_name, main_usernames, home_address, city, zip_postal, state_province, country, data)
-                  VALUES(?,?,?,?,?,?,?,?) '''
+        sql = ''' INSERT INTO entries(full_name, date_of_birth, phone_number, email, home_address, city, zip_postal, 
+                  state_province, country, facebook, twitter, instagram, linkedin, work_job_name, work_address, 
+                  work_position, other_work_info, main_usernames, data, image)
+                  VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) '''
         cursor = conn.cursor()
         cursor.execute(sql, entry)
         conn.commit()
@@ -94,13 +108,48 @@ def display_entries(conn):
 def print_entry(row):
     print(f"\nEntry ID: {row[0]}")
     print(f"Full Name: {row[1]}")
-    print(f"Main Usernames: {row[2]}")
-    print(f"Home Address: {row[3]}")
-    print(f"City: {row[4]}")
-    print(f"Zip/Postal: {row[5]}")
-    print(f"State/Province: {row[6]}")
-    print(f"Country: {row[7]}")
-    print(f"Data: {row[8]}")
+    print(f"Date of Birth: {row[2]}")
+    print(f"Phone Number: {row[3]}")
+    print(f"Email: {row[4]}")
+    print(f"Home Address: {row[5]}")
+    print(f"City: {row[6]}")
+    print(f"Zip/Postal: {row[7]}")
+    print(f"State/Province: {row[8]}")
+    print(f"Country: {row[9]}")
+    print(f"Facebook: {row[10]}")
+    print(f"Twitter: {row[11]}")
+    print(f"Instagram: {row[12]}")
+    print(f"LinkedIn: {row[13]}")
+    print(f"Work Job Name: {row[14]}")
+    print(f"Work Address: {row[15]}")
+    print(f"Work Position: {row[16]}")
+    print(f"Other Work Info: {row[17]}")
+    print(f"Main Usernames: {row[18]}")
+    print(f"Data: {row[19]}")
+    print(f"Image: {row[20]}")
+
+def update_entry(conn, entry):
+    try:
+        sql = ''' UPDATE entries SET full_name = ?, date_of_birth = ?, phone_number = ?, email = ?, home_address = ?, city = ?, 
+                  zip_postal = ?, state_province = ?, country = ?, facebook = ?, twitter = ?, instagram = ?, linkedin = ?, 
+                  work_job_name = ?, work_address = ?, work_position = ?, other_work_info = ?, main_usernames = ?, data = ?, 
+                  image = ? WHERE id = ?'''
+        cursor = conn.cursor()
+        cursor.execute(sql, entry)
+        conn.commit()
+        print(f'Entry updated: {entry[-1]}')
+    except Error as e:
+        print(e)
+
+def delete_entry(conn, id):
+    try:
+        sql = ''' DELETE FROM entries WHERE id = ? '''
+        cursor = conn.cursor()
+        cursor.execute(sql, (id,))
+        conn.commit()
+        print(f'Entry deleted: {id}')
+    except Error as e:
+        print(e)
 
 def main():
     print_banner()
@@ -116,26 +165,77 @@ def main():
             print("\nOptions:")
             print("1 - Add new entry")
             print("2 - Display entry")
-            print("3 - Exit")
+            print("3 - Edit entry")
+            print("4 - Delete entry")
+            print("5 - Exit")
 
             choice = int(input("\nEnter your choice: "))
 
             if choice == 1:
                 print("\nEnter the details below:")
                 full_name = input("Full Name: ")
-                main_usernames = input("Main Usernames: ")
+                date_of_birth = input("Date of Birth: ")
+                phone_number = input("Phone Number: ")
+                email = input("Email: ")
                 home_address = input("Home Address: ")
                 city = input("City: ")
                 zip_postal = input("Zip/Postal: ")
                 state_province = input("State/Province: ")
                 country = input("Country: ")
+                facebook = input("Facebook: ")
+                twitter = input("Twitter: ")
+                instagram = input("Instagram: ")
+                linkedin = input("LinkedIn: ")
+                work_job_name = input("Work Job Name: ")
+                work_address = input("Work Address: ")
+                work_position = input("Work Position: ")
+                other_work_info = input("Other Work Info: ")
+                main_usernames = input("Main Usernames: ")
                 data = input("Data (all other information): ")
+                image = input("Image (please provide path to image file): ")  
 
-                entry = (full_name, main_usernames, home_address, city, zip_postal, state_province, country, data)
+                entry = (full_name, date_of_birth, phone_number, email, home_address, city, zip_postal, state_province, 
+                         country, facebook, twitter, instagram, linkedin, work_job_name, work_address, work_position, 
+                         other_work_info, main_usernames, data, image)
                 insert_entry(conn, entry)
+
             elif choice == 2:
                 display_entries(conn)
+
             elif choice == 3:
+                id = int(input("Enter the ID of the entry you want to edit: "))
+                print("\nEnter the updated details below:")
+                full_name = input("Full Name: ")
+                date_of_birth = input("Date of Birth: ")
+                phone_number = input("Phone Number: ")
+                email = input("Email: ")
+                home_address = input("Home Address: ")
+                city = input("City: ")
+                zip_postal = input("Zip/Postal: ")
+                state_province = input("State/Province: ")
+                country = input("Country: ")
+                facebook = input("Facebook: ")
+                twitter = input("Twitter: ")
+                instagram = input("Instagram: ")
+                linkedin = input("LinkedIn: ")
+                work_job_name = input("Work Job Name: ")
+                work_address = input("Work Address: ")
+                work_position = input("Work Position: ")
+                other_work_info = input("Other Work Info: ")
+                main_usernames = input("Main Usernames: ")
+                data = input("Data (all other information): ")
+                image = input("Image (please provide path to image file): ")
+
+                entry = (full_name, date_of_birth, phone_number, email, home_address, city, zip_postal, state_province, 
+                         country, facebook, twitter, instagram, linkedin, work_job_name, work_address, work_position, 
+                         other_work_info, main_usernames, data, image, id)  # ID added at the end
+                update_entry(conn, entry)
+
+            elif choice == 4:
+                id = int(input("Enter the ID of the entry you want to delete: "))
+                delete_entry(conn, id)
+
+            elif choice == 5:
                 break
             else:
                 print("Invalid choice. Please try again.")
